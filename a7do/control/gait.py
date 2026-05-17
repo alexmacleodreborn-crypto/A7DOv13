8a42993df1c15d9d1308ef4fddb3bfee3cd424d7
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-05-17 15:29:45 +0100

    Update gait.py

--- a7do/control/gait.py
+++ a7do/control/gait.py
@@ -2,6 +2,13 @@ import math
 
 def update_gait_phase(phi, omega, dt):
     phi += omega * dt
-    if phi > 2 * math.pi:
+    if phi >= 2 * math.pi:
         phi -= 2 * math.pi
     return phi
+
+
+def swing_leg_target(phi, step_length):
+    """
+    Simple forward swing trajectory.
+    """
+    return step_length * math.sin(phi)
