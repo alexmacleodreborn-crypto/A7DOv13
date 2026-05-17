3b88b286855cd9df32cc996a519182acb4313105
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-05-17 15:46:19 +0100

    Update inverted_pendulum.py

--- a7do/physics/inverted_pendulum.py
+++ a7do/physics/inverted_pendulum.py
@@ -1,10 +1,9 @@
-import math
-
-def step_inverted_pendulum(x, x_dot, x_foot, g, l, dt):
+def step_inverted_pendulum(x, x_dot, x_support, tau, g, l, m, dt):
     """
-    x_ddot = (g/l) * (x - x_foot)
+    Controlled linear inverted pendulum:
+    x_ddot = (g/l)(x - x_support) + tau/(ml)
     """
-    x_ddot = (g / l) * (x - x_foot)
+    x_ddot = (g / l) * (x - x_support) + tau / (m * l)
     x_dot += x_ddot * dt
     x += x_dot * dt
     return x, x_dot
