4f908154670f85e420c65c36cabbe7ac9b99b27b
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-05-17 18:52:49 +0100

    Update body.py

--- body.py
+++ body.py
@@ -1,10 +1,6 @@
 # body.py
 def default_body():
     return {
-        "limb_lengths": {
-            "thigh": 0.12, "shank": 0.12, "foot": 0.08,
-            "torso": 0.18, "upperarm": 0.10, "forearm": 0.10
-        },
         "segment_mass": {
             "thigh": 3.5, "shank": 1.6, "foot": 1.0,
             "torso": 20.0, "head": 5.0,
